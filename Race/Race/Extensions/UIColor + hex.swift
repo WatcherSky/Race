d5746f2aaaa7b83b34f1bb6761b826ed9be87d00
WatcherSky <theworld.com@mail.ru>
2021-04-28 23:00:03 +0600

    Add UIColor extension

---
+++ Race/Race/Extensions/UIColor + hex.swift
@@ -0,0 +1,18 @@
+//  UIColor + hex.swift
+//  Race
+//
+//  Created by Владимир on 26.04.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(hex: Int) {
+        let red = (hex & 0xFF0000) >> 16
+        let green = (hex & 0x00FF00) >> 8
+        let blue = (hex & 0x0000FF)
+
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+}
