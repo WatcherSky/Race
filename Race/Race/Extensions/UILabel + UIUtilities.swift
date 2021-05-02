d9ee33d2f1f880926a6d9645a4b5ecc75fab7acf
WatcherSky <theworld.com@mail.ru>
2021-05-02 16:45:57 +0600

    add remain Hometask

---
+++ Race/Race/Extensions/UILabel + UIUtilities.swift
@@ -0,0 +1,20 @@
+// UILabel + UIUtilities.swift
+//  Race
+//
+//  Created by Владимир on 26.04.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    func applyCornerRadius(_ radius: CGFloat) {
+        clipsToBounds = true
+        layer.cornerRadius = radius
+    }
+    func makeShadow (opacity: Float, offSet: CGSize, radius: CGFloat) {
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = offSet
+        layer.shadowRadius = radius
+    }
+}
