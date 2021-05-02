27011557550db7811131b39c5d0ba2be8ec81a49
WatcherSky <theworld.com@mail.ru>
2021-05-02 17:13:25 +0600

    Revert "Revert "add remain Hometask""
    This reverts commit 0266e43196e15ecc7c0a2a252556aa885a77f947.

--- Race/Race/ViewController.swift
+++ Race/Race/ViewController.swift
@@ -8,15 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction private func startButton(_ sender: UIButton) {
-        let storyboard = UIStoryboard(name: "Start", bundle: Bundle.main)
-        let destinationToStart = storyboard.instantiateViewController(identifier: "StartViewController")
-        present(destinationToStart, animated: true, completion: nil)
+        let viewController = StartViewController.instantiate()
+        present(viewController, animated: true, completion: nil)
     }
 }
